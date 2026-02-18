a52403ee032eeed229760d745cf26a59e0d59288
Nestor Gutierrez <nestorg456k@outlook.com>
2026-02-18 10:46:07 -0500

    feat: add bootcamp report persistence port

---
+++ src/main/java/com/onclass/reporte/domain/spi/BootcampReportPersistencePort.java
@@ -0,0 +1,13 @@
+package com.onclass.reporte.domain.spi;
+
+import com.onclass.reporte.domain.model.BootcampReport;
+import reactor.core.publisher.Mono;
+
+public interface BootcampReportPersistencePort {
+
+    Mono<BootcampReport> save(BootcampReport report);
+
+    Mono<BootcampReport> findByBootcampId(String bootcampId);
+
+    Mono<BootcampReport> addEnrollment(String bootcampId, BootcampReport.EnrollmentData enrollment);
+}
